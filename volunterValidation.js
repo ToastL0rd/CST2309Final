8f9684b873af0f20342c9998216aeb0b8808baae
ToastL0rd <84319883+ToastL0rd@users.noreply.github.com>
2024-05-08 03:55:56 -0400

    Add files via upload

---
+++ volunterValidation.js
@@ -0,0 +1,24 @@
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", firstNameValidation);
+submit.addEventListener("click", lastNameValidation);
+
+// If there isn't a first and last name in the fields, display a custom validation error
+// If First Name field (id firstName) is empty, display custom valid message
+function firstNameValidation () {
+    let firstName = document.getElementById("firstName");
+    if (firstName.validity.valueMissing) {
+        firstName.setCustomValidity("Enter your first name.");
+    } else {
+        firstName.setCustomValidity("");
+    }
+}
+
+function lastNameValidation () {
+    let lastName = document.getElementById("lastName");
+    if (lastName.validity.valueMissing) {
+        lastName.setCustomValidity("Enter your last name.");
+    } else {
+        lastName.setCustomValidity("");
+    }
+}
